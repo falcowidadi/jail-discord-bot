4988ce5a70e3dab209f65e568990d34e20b2d1a7
falcowidadi <163455308+falcowidadi@users.noreply.github.com>
2025-02-16 08:33:13 +0000

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ Copyright (c) 2024 Wick Studio
 
 
 module.exports = {
-    token: 'MTM0MDQwOTc4ODU5NjQ5MDM1Mg.GdjMpL.li-1wvuLy2JbJeuKFh3WG6nuhyzWWfNo9sSP0E', // توكن
+    token: 'MTM0MDQwOTc4ODU5NjQ5MDM1Mg.Gmira8.32r-ZRZGjjj_C0aMNX5iY-VZfeNnH-beSXWjiE', // توكن
     jailRoleId: '1337161175171072151', // ايدي رتبة السجن
     jailReportChannelId: '1337550678708588544', // ايدي روم الابلاغات
     adminRoleId: '1337386230417063968', // ايدي رتبة الادمن لاستخدام الاوامر
